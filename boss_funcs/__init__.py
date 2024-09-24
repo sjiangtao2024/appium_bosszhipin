eb015a3d9fd3f736ce0cb4fc4137981379a13d90
sjiangtao2024 <163701970+sjiangtao2024@users.noreply.github.com>
2024-09-24 13:51:54 +0800

    Delete boss_funcs directory

--- boss_funcs/__init__.py
+++
@@ -1,6 +0,0 @@
-from .jobcardview import get_expected_jobs
-from .jobcardclick import jobs_apply,retrive_expected_jobs,retrive_expected_jobs_test,check_mid_tab,retrive_part_time_jobs
-from .scrollups import scroll_up_cardgroup,scroll_up,scroll_right_expected_job,scroll_left_expected_job
-from .app_log import BossLog
-from .timer import timer
-from .jobs_data import jobs_data_output
